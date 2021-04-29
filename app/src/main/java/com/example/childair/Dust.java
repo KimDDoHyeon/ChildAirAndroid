f9c85c33b1682279ad765d5b5af8f0f0009dc37c
KimDDoHyeon <80474842+KimDDoHyeon@users.noreply.github.com>
2021-04-29 18:06:43 +0900

    게시판 및 미세먼지 레이아웃 추가

---
+++ app/src/main/java/com/example/childair/Dust.java
@@ -0,0 +1,14 @@
+package com.example.childair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dust extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dust);
+    }
+}
