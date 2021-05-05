e58f05ee31fefa0bdb93771bb88f754c61fe6c87
gywp <gywp1217@gmail.com>
2021-05-05 22:28:13 +0900

    sds
    sd

--- app/src/main/java/com/example/childair/Result.java
+++ app/src/main/java/com/example/childair/Result.java
@@ -1,8 +1,11 @@
 package com.example.childair;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Result extends AppCompatActivity {
 
@@ -10,5 +13,42 @@ public class Result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ImageButton imageButton =findViewById(R.id.imageButton1);
+        ImageButton imageButton2 =findViewById(R.id.imageButton2);
+        ImageButton imageButton3 =findViewById(R.id.imageButton3);
+        ImageButton imageButton4 =findViewById(R.id.imageButton4);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Result.this, ChildBoard.class);
+                startActivity(i);
+            }
+        });
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Result.this, Calendar.class);
+                startActivity(i);
+            }
+        });
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Result.this, FoodMenu.class);
+                startActivity(i);
+            }
+        });
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Result.this, FoodMenu.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
+
 }
