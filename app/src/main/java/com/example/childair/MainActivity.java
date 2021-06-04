d79b7c418b9774f057ef22700a716edecb5fa11d
KimDDoHyeon <80474842+KimDDoHyeon@users.noreply.github.com>
2021-06-04 14:11:33 +0900

    레이아웃 작업 완료

--- app/src/main/java/com/example/childair/MainActivity.java
+++ app/src/main/java/com/example/childair/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.childair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton airBtn = findViewById(R.id.btnAir);
+        ImageButton calBtn = findViewById(R.id.btnCalen);
+
+        airBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent airgo = new Intent(getApplicationContext(), Dust.class);
+                startActivity(airgo);
+            }
+        });
+
+        calBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calgo = new Intent(getApplicationContext(), Calendar.class);
+                startActivity(calgo);
+            }
+        });
 
     }
 }
